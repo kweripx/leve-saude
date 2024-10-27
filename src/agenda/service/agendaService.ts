3cb950c1b0e3c69ef4c8dcc9ded1db39b1432c1d
Vitor <vitor.hugoqueiroz11@gmail.com>
2024-10-26 22:32:19 -0300

    style(agenda): fix code formatting in agendaService

--- src/agenda/service/agendaService.ts
+++ src/agenda/service/agendaService.ts
@@ -1,8 +1,8 @@
-import { IDoctor } from "../interface/IDoctor";
-import { doctorsMock } from "../mocks/doctorsScheduleMock";
+import { IDoctor } from '../interface/IDoctor';
+import { doctorsMock } from '../mocks/doctorsScheduleMock';
 
 export class AgendaService {
-    public async getAgendas(): Promise<IDoctor[]> {
-        return doctorsMock
-    }
+  public async getAgendas(): Promise<IDoctor[]> {
+    return doctorsMock;
+  }
 }
